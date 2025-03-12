849f5035fe86101f16359f45e951df46d4d9e86d
Maxime Moisant <maxime@maximemoisant.fr>
2025-03-12 12:25:50 +0100

    cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_11_102054) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_12_100855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_11_102054) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "poster_url"
+    t.string "show_type"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,46 @@
 require 'open-uri'
 require 'json'
 
-movies = JSON.load(URI.open("https://tmdb.lewagon.com/movie/top_rated"))['results']
-movies.each do |movie|
+ids = [916224, 372058, 671, 808, 22, 11, 8587, 85, 118340, 424694]
+
+#Suzume 916224
+#Your name 372058
+#Harry Potter 671
+#Shrek 808
+#Peaky blinders 60574 (TV)
+#Pirates des caraibes 22
+#Stranger Things 66732 (TV)
+#Attaques des titans (tokyo ghoul/demon slayer)
+#Stars wars 11
+#Squid game 93405 (TV)
+#Roi lion 8587
+#Indiana Jones 85
+#Game of thrones 1399 (TV)
+#Casa de papel 71446 (TV)
+#Guardians of the Galaxy 118340
+#Bohemian Rapsody 424694
+
+ids.each do |id|
+  movie = JSON.load(URI.open("https://tmdb.lewagon.com/movie/#{id}"))
   Show.create!(
     name: movie['title'],
+    show_type: "movie",
     poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}",
-  )
+    )
   puts "Création (Film): #{movie['title']}"
 end
 
-series = JSON.load(URI.open("https://tmdb.lewagon.com/tv/top_rated"))['results']
-series.each do |serie|
+ids = [66732, 93405, 60574, 1399, 71446]
+
+
+ids.each do |id|
+  serie = JSON.load(URI.open("https://tmdb.lewagon.com/tv/#{id}"))
   Show.create!(
     name: serie['title'],
+    show_type: "tv",
     poster_url: "https://image.tmdb.org/t/p/original#{serie['poster_path']}",
   )
   puts "Création (Serie): #{serie['name']}"
 end
-
 
 #TODO voir pour ajouter les musiques dans la seed
