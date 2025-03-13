86e550b77689df5d2c336e5be3654d225ecc4c28
Maxime Moisant <maxime@maximemoisant.fr>
2025-03-13 16:43:03 +0100

    update cards Passe 2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,11 +2,9 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import { Application } from "@hotwired/stimulus"
-import ToggleMusicController from "./controllers/toggle_music_controller"
 import DropdownController from "./controllers/dropdown_controller"
 import DropdownSearchController from "./controllers/dropdown_search_controller"
 
 const application = Application.start()
-application.register("toggle-music", ToggleMusicController)
 application.register("dropdown", DropdownController)
 application.register("dropdown-search", DropdownSearchController);
